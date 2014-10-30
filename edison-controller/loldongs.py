ff7df45b7cd8b8ecfe4566c6c9a1299da413a5f7
Matt Joyce <matt@nycresistor.com>
2014-10-30 18:34:49 -0400

    adding edison controller code

---
+++ edison-controller/loldongs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python  
+      
+import mraa  
+import time  
+import sys
+      
+x = mraa.Gpio(2)  
+z = mraa.Gpio(4)
+x.dir(mraa.DIR_OUT)  
+      
+while True:  
+    try:
+        val = z.read()
+
+        if ( val == 1 ):
+            x.write(1)
+        else:
+            x.write(0)
+
+        # print "%d" % val
+       
+    except:
+        # print "dongs!"
+        sys.exit(2)
