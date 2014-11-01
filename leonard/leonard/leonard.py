75649f2b4f2a74d130673665f04be04408d2ec76
Matt Joyce <matt@nycresistor.com>
2014-10-31 20:58:54 -0400

    adding some sensor reading code for the shitty grove temp sensor

--- leonard/leonard/leonard.py
+++ leonard/leonard/leonard.py
@@ -5,17 +5,23 @@
   -- target platform Intel Edison
 """
 
-import mraa  
-import time  
+import mraa
+import time
 import sys
-      
-x = mraa.Gpio(2)  
+
+x = mraa.Gpio(2)
 z = mraa.Gpio(4)
-x.dir(mraa.DIR_OUT)  
-      
-while True:  
+x.dir(mraa.DIR_OUT)
+
+while True:
     try:
         val = z.read()
+
+        x = mraa.Aio(0)
+        t = x.read()
+        resistance=(float)(1023-t)*10000/t
+        ctemp=(float)(1/(log(resistance/10000)/3975+1/298.15)-273.15)
+        ftemp=ctemp*9/5+32
 
         if ( val == 1 ):
             x.write(1)
@@ -23,7 +29,8 @@ while True:
             x.write(0)
 
         # print "%d" % val
-       
+
     except:
         # print "dongs!"
         sys.exit(2)
+
